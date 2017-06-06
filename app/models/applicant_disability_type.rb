e9ae7a5b1ecddff2aa6e7ddf60fcbc847da9cd77
Monwabisi <monwabisi@piglet.littlepig.cc>
2017-06-06 14:50:15 +0200

    changes according to feedback

--- app/models/applicant_disability_type.rb
+++ app/models/applicant_disability_type.rb
@@ -3,5 +3,5 @@ class ApplicantDisabilityType < ApplicationRecord
   belongs_to :applicant_disability
 
   validates :applicant_disability_id, presence: true
-  validates :Disability_Type, presence: true
+  # validates :Disability_Type, presence: true
 end

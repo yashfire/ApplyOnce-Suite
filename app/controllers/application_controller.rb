30442d496b764f5e1da024118ad3e1d7f2c84e39
Calvin-Seidle <seidlecalvin@gmail.com>
2017-06-14 10:58:31 +0200

    added validations on registration form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 	def configure_permitted_parameters
-		devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:username, :id_number, :email, :password, :remember_me)}
+		devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:username, :id_number, :email, :password, :password_confirmation, :remember_me)}
 		devise_parameter_sanitizer.permit(:sign_in) {|u| u.permit(:email, :password, :remember_me)}
-		devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:username, :id_number, :email, :password, :remember_me, :current_password)}
+		devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:username, :id_number, :email, :password, :password_confirmation, :remember_me, :current_password)}
 	end	
 end

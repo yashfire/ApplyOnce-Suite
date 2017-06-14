d8c9a54c6f380cffbf71b0c80e31e53ca382abcf
Calvin-Seidle <seidlecalvin@gmail.com>
2017-06-14 13:24:21 +0200

    development and production settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,17 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-# config.action_mailer.default_url_options = { host: '192.168.1.20', port: 
-# 3000 }
-  config.action_mailer.default_url_options = { :host => "localhost:3000" }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'littlepig.cc',
-    user_name:            'seidlecalvin',
-    password:             'seidlc06',
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+ config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -38,7 +28,7 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true####
+  config.action_mailer.raise_delivery_errors = false####
 
   config.action_mailer.perform_caching = false
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,15 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'littlepig.cc',
+    user_name:            'seidlecalvin',
+    password:             'seidlc06',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
   # Code is not reloaded between requests.
   config.cache_classes = true
 

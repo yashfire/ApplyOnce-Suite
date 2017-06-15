dbae05d2e70b2a57047287d486eea066e66ea3c1
Calvin-Seidle <seidlecalvin@gmail.com>
2017-06-15 09:02:59 +0200

    develepment and production settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,16 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
- config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
+#config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { :host => "192.168.1.20:3000" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'littlepig.cc',
+    user_name:            'seidlecalvin',
+    password:             'seidlc06',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.

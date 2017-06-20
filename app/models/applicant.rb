1dc0892677e291b15bcc31fd3b59dd935c188ba6
Calvin-Seidle <seidlecalvin@gmail.com>
2017-06-20 08:42:01 +0200

    minor changes

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -18,7 +18,7 @@ class Applicant < ApplicationRecord
   #validates_format_of :id_number,:with =>/\A(?<Year>[0-9][0-9])(?<Month>([0][1-9])|([1][0-2]))(?<Day>([0-2][0-9])|([3][0-1]))(?<Gender>[0-9])(?<Series>[0-9]{3})(?<Citizenship>[0-9])(?<Uniform>[0-9])(?<Control>[0-9])\z/
   validates_format_of :id_number, :with => /\A(((\d{2}((0[13578]|1[02])(0[1-9]|[12]\d|3[01])|(0[13456789]|1[012])(0[1-9]|[12]\d|30)|02(0[1-9]|1\d|2[0-8])))|([02468][048]|[13579][26])0229))(( |-)(\d{4})( |-)(\d{3})|(\d{7}))\z/
   
-  validates_uniqueness_of :id_number
+  validates_uniqueness_of :id_number, length: { minimum: 13, maximum: 13 }
   validates_uniqueness_of :email
   validates_uniqueness_of :username
 
